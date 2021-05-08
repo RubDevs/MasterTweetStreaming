2532f6001864a8a27267eaf97508c5d5cd22b16b
Rubén Hernández <69823633+RubDevs@users.noreply.github.com>
2021-05-08 14:01:03 -0500

    Unit testing added (#30)

--- api/components/tweet/network.js
+++ api/components/tweet/network.js
@@ -2,17 +2,20 @@ const response = require("../../../network/response");
 const express = require("express");
 const Controller = require("./index");
 
-const router = express.Router();
+function tweetsApi(app) {
+  const router = express.Router();
+  app.use("/tweets", router);
 
-router.get("/", list);
+  router.get("/", list);
 
-//Internal middleware
-async function list(req, res, next) {
-  Controller.list()
-    .then((tweets) => {
-      response.success(req, res, tweets, 200);
-    })
-    .catch(next);
+  //Internal middleware
+  async function list(req, res, next) {
+    Controller.list()
+      .then((tweets) => {
+        response.success(req, res, tweets, 200);
+      })
+      .catch(next);
+  }
 }
 
-module.exports = router;
+module.exports = tweetsApi;
