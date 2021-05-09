7b37ac66c3d1b873e668d8b1824c394bb441f98c
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-05-09 18:50:17 -0500

    Integration test done

--- test/integration.api.component.tweet.test.js
+++ test/integration.api.component.tweet.test.js
@@ -4,9 +4,7 @@ const expect = require("chai").expect;
 const tweetAPI = require("../api/components/tweet/network");
 const testServer = require("../utils/testServer");
 const assert = require("assert");
-const tweetsMock = require("../mocks/Tweets");
 
-const app = express();
 describe("GET /tweets", () => {
   const request = testServer(tweetAPI);
 
@@ -21,13 +19,9 @@ describe("GET /tweets", () => {
     });
   });
 
-  it("Should responds with the list of tweets", function (done) {
+  it("Should responds with 50 tweets", function (done) {
     request.get("/tweets").end((error, response) => {
-      assert.deepStrictEqual(response.body, {
-        error: false,
-        status: 200,
-        body: tweetsMock,
-      });
+      expect(response.body.length, 50);
       done();
     });
   });
---
+++ test/unit.api.component.tweet.test.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const expect = require("chai").expect;
+const tweetAPI = require("../api/components/tweet/network");
+const testServer = require("../utils/testServer");
+const assert = require("assert");
+const tweetsMock = require("../mocks/Tweets");
+
+describe("GET /tweets", () => {
+  const request = testServer(tweetAPI);
+
+  it("Should respond with status 200", (done) => {
+    request.get("/tweets").expect(200, done);
+  });
+
+  it("Should respond with no error", (done) => {
+    request.get("/tweets").end((error, response) => {
+      assert.strictEqual(error, null);
+      done();
+    });
+  });
+
+  it("Should responds with the list of tweets", function (done) {
+    request.get("/tweets").end((error, response) => {
+      assert.deepStrictEqual(response.body, {
+        error: false,
+        status: 200,
+        body: tweetsMock,
+      });
+      done();
+    });
+  });
+});
